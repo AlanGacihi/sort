35efeb1e62da91ea52cc35f618487b251dd4714d
AlanGacihi <alanmamicha@gmail.com>
2022-10-24 09:00:45 +0300

    first commit

--- sort.cpp
+++ sort.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <sys/types.h>
 #include <pwd.h>
+#include <time.h>
 #include <unistd.h>
 
 // Add stuff to the set to wast time and space
@@ -22,6 +23,8 @@ int main()
 {
     struct passwd *p;
     uid_t  uid;
+    clock_t start, end;
+    double duration;
     
     if ((p = getpwuid(uid = geteuid())) == NULL)
         std::cerr << "getpwuid() error" << std::endl;
@@ -30,7 +33,17 @@ int main()
     std::cout << "Parent procees ID: " << getppid() << std::endl;
     std::cout << "Owner's user ID: " << (int) p->pw_uid << std::endl;
     std::cout << "Owner's group ID: " << (int) p->pw_gid << std::endl;
-    std::cout << "Name: " << p->pw_name << std::endl;
+
+    for (int i = 0; i <= 5; i++) {
+        std::cout << "\nPriority " << i << std::endl;
+        start = clock();
+        chase_tail();
+        end = clock();
+        duration = (double(end - start)) / CLOCKS_PER_SEC;
+        printf("CPU time used: %.2f seconds.\n");
 
 
+    }
+
+    return 0;
 }
