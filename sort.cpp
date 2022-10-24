bf5f9c30d92a3e51ee1f5cbe29ba2ae0f8ef6293
AlanGacihi <alanmamicha@gmail.com>
2022-10-24 08:42:19 +0300

    first commit

--- sort.cpp
+++ sort.cpp
@@ -1,6 +1,9 @@
 #include <set>
 #include <iostream>
 #include <cstdlib>
+#define _POSIX_SOURCE
+#include <sys/types.h>
+#include <pwd.h>
 #include <unistd.h>
 
 // Add stuff to the set to wast time and space
@@ -18,8 +21,14 @@ void chase_tail()
 
 int main()
 {
+    struct passwd *p;
+    uid_t  uid;
+    
+    if ((p = getpwuid(uid = geteuid())) == NULL)
+        perror("getpwuid() error");
+    
     std::cout << "Process ID: " << getpid() << std::endl;
     std::cout << "Parent procees ID: " << getppid() << std::endl;
-    std::cout << "Owner's user ID: " << geteuid() << std::endl;
-    std::cout << "Owner's group ID: " << getgid() << std::endl;
+    std::cout << "Owner's user ID: " << (int) p->pw_uid << std::endl;
+    std::cout << "Owner's group ID: " << (int) p->pw_gid << std::endl;
 }
